e23814e5888f0147c2c514d6b92c911b18dedba4
David Morcuende <david.morcuende.c@gmail.com>
2018-06-26 00:17:17 +0200

    Added Dockerfile to deploy a container with the recomend server

--- recommend/recommender_server.py
+++ recommend/recommender_server.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import numpy as np
@@ -235,5 +237,5 @@ api.add_resource(MetadataImproved, '/metadata_i')
 
 if __name__ == "__main__":
     main()
-    app.run(port="5005")
+    app.run(host='0.0.0.0', port="5005")
 
